8dd2d5b8a60012ecaf471f16962d9c93faa75c54
Tani Ifegbesan <ifegbesan6@gmail.com>
2025-10-05 13:49:28 +0100

    chore: string, int, arr variables

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ func main() {
 	conferenceName := "Tanisty Conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
+	// var bookings [50]string
+	var bookings = []string{}
 
 	fmt.Printf("Welcome to %v booking application \n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available \n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
-
-	var bookings [50]string
 
 	var firstName string
 	var lastName string
@@ -31,13 +31,11 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets -= userTickets
-	bookings[0] = firstName + " " + lastName
-
-	fmt.Printf("The whole array: %v\n", bookings)
-	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("Array type: %T\n", bookings)
-	fmt.Printf("Array length: %v\n", len(bookings))
+	// bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName+" "+lastName)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings: %v \n", bookings)
 }
